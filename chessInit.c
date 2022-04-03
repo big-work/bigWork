583198e106f7fc39e56a715788dbbd87117e04a4
KKKKKaoriiiii <1714726717@qq.com>
2022-04-03 13:06:38 +0800

    the second week commit

--- chessInit.c
+++ chessInit.c
@@ -50,3 +50,24 @@ Chessboard** createChessboard(int x, int y, int MineNum)
 	}
 	return myCBList;
 }
+
+Chessboard** markOneChess(Chessboard** myCBList, int cx, int cy, int x, int y) {
+	int winFlag = 1;
+	if (myCBList[x][y].tagOrNot = 1) {
+		printf("%s", "指定的格子已被标记!");
+		return myCBList;
+	}
+	myCBList[x][y].tagOrNot = 1;
+	for (int i = 0; i < cx; i++) {
+		for (int t = 0; t < cy; t++) {
+			Chessboard p = myCBList[i][t];
+			if (p.tagOrNot == 1 && p.flag == 0 || p.tagOrNot == 0 && p.flag == 1) {
+				winFlag = 0;
+				break;
+			}
+		}
+	}
+	if (winFlag)
+		return NULL;
+	return myCBList;
+}
