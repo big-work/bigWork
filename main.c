eae4f0cbb325b1ebf66de38ca129ffb2e449f8d1
KKKKKaoriiiii <1714726717@qq.com>
2022-05-03 13:56:13 +0800

    e

--- main.c
+++ main.c
@@ -7,30 +7,30 @@ int main() {
 	stu = createChessboard(10, 9, 10);
 	stu = makeChessboard(stu, 10, 9);
 	stu[3][4].tagOrNot = 1;
-	/*for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 10; i++) {
 		for (int t = 0; t < 9; t++) {
 			printf("%d\t", stu[i][t].flag);
 		}
 		printf("\n");
 	}
-	printf("\n");*/
-	/*for (int i = 0; i < 10; i++) {
+	printf("\n");
+	for (int i = 0; i < 10; i++) {
 		for (int t = 0; t < 9; t++) {
 			printf("%d\t", stu[i][t].nearbyMineNum);
 		}
 		printf("\n");
-	}*/
-	/*printf("\n");*/
+	}
+	printf("\n");
 	do
 	{
 		stu = drawOneChess(stu, 10, 9, 2, 5);
 	} while (stu == NULL);
-	/*for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 10; i++) {
 		for (int t = 0; t < 9; t++) {
 			printf("%d\t", stu[i][t].drawOrNot);
 		}
 		printf("\n");
-	}*/
+	}
 
 
 	//  以上stu即为已经绘制好的棋盘，在此处调试Chessprint
